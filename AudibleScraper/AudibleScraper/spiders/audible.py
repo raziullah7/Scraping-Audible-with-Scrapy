3565d1b2dcd6f83db9e4080c1314dcfc9ff4c4cc
Razi Ullah <raziullah913@gmail.com>
2024-01-08 02:05:18 +0500

    first commit

---
+++ AudibleScraper/AudibleScraper/spiders/audible.py
@@ -0,0 +1,27 @@
+import scrapy
+
+
+class AudibleSpider(scrapy.Spider):
+    name = "audible"
+    allowed_domains = ["www.audible.com"]
+    start_urls = ["https://www.audible.com/search"]
+
+    def parse(self, response):
+        product_container = response.xpath("//div[@class='adbl-impression-container ']/div/span/ul/li")
+        
+        for product in product_container:
+            book_author = product.xpath(".//li[contains(@class, 'authorLabel')]/span/a/text()").getall()
+            book_length = product.xpath(".//li[contains(@class, 'runtimeLabel')]/span/text()").get()
+            book_title = product.xpath(".//h3[contains(@class, 'bc-heading')]/a/text()").get()
+            
+            yield {
+                'title': book_title,
+                'author(s)': book_author,
+                'length': book_length,
+            }
+
+        pagination = response.xpath("//ul[contains(@class, 'pagingElements')]/li")
+        next_page_url = pagination.xpath(".//span[contains(@class, 'nextButton')]/a/@href").get()
+        
+        if next_page_url:
+            yield response.follow(url=next_page_url, callback=self.parse)
