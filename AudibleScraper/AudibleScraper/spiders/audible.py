419efb5d28e22ebea7dd9c410d6ecdd6d8b01f19
Razi Ullah <raziullah913@gmail.com>
2024-01-25 17:02:44 +0500

    understood how to change user agent in from scrapy to the given website's preference

--- AudibleScraper/AudibleScraper/spiders/audible.py
+++ AudibleScraper/AudibleScraper/spiders/audible.py
@@ -4,7 +4,10 @@ import scrapy
 class AudibleSpider(scrapy.Spider):
     name = "audible"
     allowed_domains = ["www.audible.com"]
-    start_urls = ["https://www.audible.com/search"]
+    # start_urls = ["https://www.audible.com/search"]
+    
+    def start_requests(self):
+        yield scrapy.Request(url='https://www.audible.com/search', callback=self.parse, headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36'})
 
     def parse(self, response):
         product_container = response.xpath("//div[@class='adbl-impression-container ']/div/span/ul/li")
@@ -18,10 +21,11 @@ class AudibleSpider(scrapy.Spider):
                 'title': book_title,
                 'author(s)': book_author,
                 'length': book_length,
+                'User-Agent': response.request.headers['User-Agent'],
             }
 
         pagination = response.xpath("//ul[contains(@class, 'pagingElements')]/li")
         next_page_url = pagination.xpath(".//span[contains(@class, 'nextButton')]/a/@href").get()
         
         if next_page_url:
-            yield response.follow(url=next_page_url, callback=self.parse)
+            yield response.follow(url=next_page_url, callback=self.parse, headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36'})
